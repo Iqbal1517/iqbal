5e65bf0a9888577aad549cc7f9f6ede850d23af7
Iqbal1517 <131316256+Iqbal1517@users.noreply.github.com>
2024-11-23 00:35:28 -0800

    mit software
    our institute

---
+++ iqbal.js
@@ -0,0 +1,15 @@
+// first we grab the 
+let a =  document.getElementById("mit");
+//    console.log(a)
+ // getting content of p element that stored in a and print on console.
+ console.log(a.textContent);
+ a.style.color="green";
+ a.style.backgroundColor="magenta";
+ a.style.border="2px double blue";
+ a.style.padding="2px 30px 2px 30px";
+ a.style.textAlign="center";
+
+ 
+ 
+
+  
